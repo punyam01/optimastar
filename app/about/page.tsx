81964c2afcc23f12d294315299137958974cb05f
Ayush Raj <163673624+bhardwajayush8210@users.noreply.github.com>
2025-04-06 18:27:04 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -92,7 +92,7 @@ export default function AboutPage() {
             >
               <div className="rounded-lg overflow-hidden shadow-xl h-full">
                 <Image
-                  src="/images/Untitled design (5).png"
+                  src="/images/Untitled design (6).png"
                   alt="OPTIMA STAR Technical Services"
                   width={800}
                   height={800}
